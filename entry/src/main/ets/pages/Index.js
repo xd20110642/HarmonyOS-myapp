f9e2afce5347cdf0c24f23c1fad2a064c2991862
xiangdong <790057064@qq.com>
2023-12-14 23:22:30 +0800

    feat：update

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,9 @@
+import Login from '../views/login/login';
+struct Index extends   {
+    constructor() { }
+    build() {
+            .height('100%')
+            .width('100%');
+    }
+}
+//# sourceMappingURL=Index.js.map
